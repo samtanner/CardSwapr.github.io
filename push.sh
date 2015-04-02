1bf084841c383b20c776755e43688c9636218bf1
Nicholas Clawson <nickclaw@gmail.com>
2015-04-02 13:29:26 -0700

    Fix github token auth

--- push.sh
+++ push.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-
+echo "Starting push..."
 cd build
 rm -rf .git
+
+echo "Creating repository..."
+echo $GIT_NAME;
+echo $GIT_EMAIL;
 git init
 git config --local --add user.name=$GIT_NAME
 git config --local --add user.email=$GIT_EMAIL
+git remote add origin "https://nickclaw:$GIT_TOKEN@github.com/cardswapr/cardswapr.github.io.git"
 
-git config --local --add github.token=$GIT_TOKEN
-
-git remote add origin https://github.com/cardswapr/cardswapr.github.io.git
+echo "Commiting changes..."
 git add -A
 git commit -m "Update blog."
+
+echo "Pushing to master..."
 git push -f origin master
+
+echo "Push completed."
