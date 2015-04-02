0a5a21edc022e95efccd73f77ee9d3cb34f41360
Nicholas Clawson <nickclaw@gmail.com>
2015-04-02 12:44:06 -0700

    Add travis-yml build process

--- push.sh
+++ push.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-cd build
-rm -rf .git
-git init
-git remote add origin https://github.com/cardswapr/cardswapr.github.io.git
-git add -A
-git commit -m "Update blog."
-git push -f origin master
+echo $TEST
+
+# cd build
+# rm -rf .git
+# git init
+# git remote add origin https://github.com/cardswapr/cardswapr.github.io.git
+# git add -A
+# git commit -m "Update blog."
+# git push -f origin master
