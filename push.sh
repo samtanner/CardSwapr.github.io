7c2f010f2fb9eeaf90a0705f44ac5c5cb84f991a
Nicholas Clawson <nickclaw@gmail.com>
2015-04-02 13:21:25 -0700

    Add push to master

--- push.sh
+++ push.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-echo $TEST
+cd build
+rm -rf .git
+git init
+git config --local --add user.name=$GIT_NAME
+git config --local --add user.email=$GIT_EMAIL
 
-# cd build
-# rm -rf .git
-# git init
-# git remote add origin https://github.com/cardswapr/cardswapr.github.io.git
-# git add -A
-# git commit -m "Update blog."
-# git push -f origin master
+git config --local --add github.token=$GIT_TOKEN
+
+git remote add origin https://github.com/cardswapr/cardswapr.github.io.git
+git add -A
+git commit -m "Update blog."
+git push -f origin master
